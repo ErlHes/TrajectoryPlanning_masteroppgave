adbb5f30bab22a8859a51e1d83bc842fdbafbf12
ErlHes <erlend.Hestvik@gmail.com>
2022-06-10 14:01:44 +0200

    Add appendix

--- MATLAB/getCPAlist.m
+++ MATLAB/getCPAlist.m
@@ -23,24 +23,27 @@ for i = vessel.current_wp:size(vessel.wp,2)-1
 %Fra vessel.eta, hvor lang tid tar det å nå neste wpt?
 %Fra neste wpt, hvor lang tid tar det å nå neste wpt? <- repeat for alle
 %wpts. Anta konstant fart hele veien.
+
+    %Find pose at current OS waypoint
     [pos_OS, vel_OS] = VesselWPReadout(vessel,i);
-    distancetonextWP = sqrt((vessel.wp(1,i+1) - pos_OS(1))^2 + ((vessel.wp(2,i+1) - pos_OS(2))^2));
-    timetonextWP = distancetonextWP / norm(vessel.nu(1:2),2); %Distanse OG time to next wp er redundant, egentlig kunne jeg klart meg med en.
-    [pos_TS, vel_TS] = whereisTS(tracks,wptstimer); % Find where TS is when OS is at current position
+    [pos_TS, vel_TS] = whereisTS(tracks,wptstimer); 
 
-    %%TODO: Skjekk *alle* relevante TS waypoints når OS følger en veldig
-    %%lang bane.
-
-    [dCPA, tCPA] = ClosestApproach(pos_OS, pos_TS, vel_OS, vel_TS);
-    dCPA = round(dCPA*1000)/1000;
-    tCPA = tCPA + wptstimer; % Blir dette rett? hvem vet.
     heading_OS = atan2(vel_OS(2),vel_OS(1));
     heading_TS = atan2(vel_TS(2),vel_TS(1));
+
+    %Do cpa check
+    [dCPA, tCPA] = ClosestApproach(pos_OS, pos_TS, vel_OS, vel_TS);
+    dCPA = round(dCPA*1000)/1000;
+    tCPA = tCPA + wptstimer; % håper dette blir rett.
     pos_OS = [pos_OS;heading_OS];
     pos_TS = [pos_TS;heading_TS];
     pos_OS_list = [pos_OS_list, pos_OS];
     pos_TS_list = [pos_TS_list, pos_TS];
 
+
+    %step forward one waypoint.
+    distancetonextWP = sqrt((vessel.wp(1,i+1) - pos_OS(1))^2 + ((vessel.wp(2,i+1) - pos_OS(2))^2));
+    timetonextWP = distancetonextWP / norm(vessel.nu(1:2),2); %Distanse OG time to next wp er redundant, egentlig kunne jeg klart meg med en.
     wptstimer = wptstimer + timetonextWP;
     dCPAlist = [dCPAlist, dCPA];
     tCPAlist = [tCPAlist, tCPA];
